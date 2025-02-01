a7af72edc0ba28bd0019265684356c7dbacc16ee
Maciek Sitkowski <msitkowski94@gmail.com>
2025-02-01 20:03:48 +0100

    fix

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,8 +1,8 @@
-import globals from 'globals'
 import pluginJs from '@eslint/js'
-import tseslint from 'typescript-eslint'
 import importPlugin from 'eslint-plugin-import'
 import pluginReact from 'eslint-plugin-react'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
 
 export default tseslint.config({
 	ignores: ['node_modules', 'dist', 'src-tauri'],
